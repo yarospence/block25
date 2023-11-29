624bba25368781ee7e9a6b4d9957d93c6c909229
Spencer Toliver <tolivercapital@gmail.com>
2023-11-29 16:17:13 -0600

    updated color picker assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-const Color = (props) => {
-  return <div className={props.color}></div>;
+import { useState } from "react";
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
 };
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="yellow" />
-        <Color color="green" />
-        <Color color="black" />
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+        <Color color="black" setSelectedColor={setSelectedColor} />
       </div>
     </div>
   );
